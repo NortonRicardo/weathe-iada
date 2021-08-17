5653159968208e062324f063b9b710e6198f7140
Norton Ricardo <norton.ricardo@siedos.com.br>
2021-08-17 19:18:33 -0300

    Create seed para user norton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#name: 'Norton Ricardo', 
+User.create(email:'nortonricardo@live.com', password:'iada2029', password_confirmation: 'iada2029')
