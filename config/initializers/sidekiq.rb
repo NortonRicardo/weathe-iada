de11d132b3ceab3a0f9496d0d04c7a3ec6d47212
NortonRicardo <nortonricardo@live.com>
2021-08-20 09:39:41 -0300

    config na mao

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,30 +1,29 @@
 # # require 'resque/server'
 # #
-# # if Rails.env.development?
-# #   Resque.redis = Redis.new(host: 'localhost', port: '6379')
-# # else
-#   uri = URI.parse(ENV['REDIS_URL'])
+# if Rails.env.development?
+#   Resque.redis = Redis.new(host: 'localhost', port: '6379')
+# else
 #   REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 #   Resque.redis = REDIS
-# # end
+# end
 
-local_redis = ENV.fetch("REDIS_URL") if ENV["REDIS_URL"].present?
+# local_redis = ENV.fetch("REDIS_URL") if ENV["REDIS_URL"].present?
 
-if local_redis.present?
-  parametros = {url: local_redis, password: 'redis'}
-else
-  parametros = {url: 'redis://localhost:6379/0'}
-end
+# if local_redis.present?
+#   paramet
+# else
+#
+# end
 
 
-def conexao_redis(parametros)
+def conexao_redis()
   Sidekiq.configure_server do |config|
-    config.redis = parametros
+    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110', password: 'pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b'}
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = parametros
+    config.redis = {url: 'redis://localhost:6379/0'}
   end
 end
 
-conexao_redis(parametros)
+conexao_redis()
