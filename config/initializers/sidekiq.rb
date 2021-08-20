e212437f2a92c3886de82751dbcf457f57cddc78
NortonRicardo <nortonricardo@live.com>
2021-08-20 13:14:18 -0300

    config 3

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@ require 'sidekiq/web'
 
 
 Sidekiq.configure_server do |config|
-   config.redis = {url: ENV['HEROKU_REDIS_IVORY_TLS_URL'], ssl_params: {verify_mode: false}}
+   config.redis = {url: ENV['HEROKU_REDIS_IVORY_TLS_URL'], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}
    # config.redis = {url: 'redis://localhost:6379/0'}
 end
 
 Sidekiq.configure_client do |config|
-  # config.redis = {url: 'redis://localhost:6379/0'}
-  config.redis = {url: ENV['HEROKU_REDIS_IVORY_TLS_URL'], ssl_params: {verify_mode: false}}
+    # config.redis = {url: 'redis://localhost:6379/0'}
+    config.redis = {url: ENV['HEROKU_REDIS_IVORY_TLS_URL'], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}
 end
