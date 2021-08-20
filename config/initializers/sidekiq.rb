5d761c55b10886d2a2f0ad30c5ef7cf21014d11a
NortonRicardo <nortonricardo@live.com>
2021-08-20 09:45:09 -0300

    Teste final

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -22,7 +22,8 @@ def conexao_redis()
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = {url: 'redis://localhost:6379/0'}
+    # config.redis = {url: 'redis://localhost:6379/0'}
+    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110', password: 'pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b'}
   end
 end
 
