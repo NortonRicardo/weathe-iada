0ee6e6aedf79a128ae8b11b7d5d8f9616ab36529
NortonRicardo <nortonricardo@live.com>
2021-08-20 12:12:05 -0300

    config final deu certo

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 require 'sidekiq/web'
 
 
-  Sidekiq.configure_server do |config|
-    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110'}
-  end
+Sidekiq.configure_server do |config|
+   config.redis = {url: 'redis://:p07d22f609e3e0441aa5c588e9bc817f042bdf5a49e99284fac29436e3491004d@ec2-184-72-149-26.compute-1.amazonaws.com:11980'}
+end
 
-  Sidekiq.configure_client do |config|
-    # config.redis = {url: 'redis://localhost:6379/0'}
-    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110'}
-  end
+Sidekiq.configure_client do |config|
+  # config.redis = {url: 'redis://localhost:6379/0'}
+  config.redis = {url: 'redis://:p07d22f609e3e0441aa5c588e9bc817f042bdf5a49e99284fac29436e3491004d@ec2-184-72-149-26.compute-1.amazonaws.com:11980'}
+end
