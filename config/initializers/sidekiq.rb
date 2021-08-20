d2bbb93d9965aac55a11ded9bd27bbf6f707be0d
NortonRicardo <nortonricardo@live.com>
2021-08-20 11:50:17 -0300

    config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,12 @@
-require "sidekiq/web"
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
 
-SIDEKIQ_REDIS_CONFIGURATION = {
-  url: ENV[ENV["REDIS_URL"]], # if one assumes that REDIS_PROVIDER indirection is reliably present
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # we must trust Heroku and AWS here
-}
 
-Sidekiq.configure_server do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
-end
+  Sidekiq.configure_server do |config|
+    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110'}
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
-end
+  Sidekiq.configure_client do |config|
+    # config.redis = {url: 'redis://localhost:6379/0'}
+    config.redis = {url: 'redis://:pbc1ad979199760ed39137274b662c0623909839fe0d0312b29804ab496e3063b@ec2-54-159-210-48.compute-1.amazonaws.com:8110'}
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 require 'sidekiq/web'
-require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
-  end
-  mount Sidekiq::Web, at: "/sidekiq"
+  mount Sidekiq::Web => '/sidekiq'
 
   resources :weather_data
   resources :weather_stations
