d37c68a195017f9de29e3b5aab1026388bba50bd
NortonRicardo <norton.ricardo@passwordinfo.com.br>
2021-11-20 15:44:47 -0300

    correção

--- app/models/import_datum.rb
+++ app/models/import_datum.rb
@@ -4,10 +4,17 @@ class ImportDatum < ApplicationRecord
   self.sequence_name = "tb_import_data_seq"
   # relationships .............................................................
   belongs_to :import, class_name: 'Import', foreign_key: :tb_import_id
-  has_one_attached :file
+  has_one_attached :file, dependent: :detach
   # validations ...............................................................
   # callbacks .................................................................
+   before_destroy :delete_file
   #
+
+
+  def delete_file
+    self.file.purge
+  end
+
   def file_dowlod
     self.file.download
   end
